39ae496ba7fe79e80750c82a9a828c86274c74f1
kboughamoura <boughamourakhaled11@gmail.com>
2021-11-28 12:56:45 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,108 @@
+<?php
+
+?>
+
+
+<html lang="en">
+
+<?php  
+include('header.php');
+?>
+     <section class="slider_section">
+      <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>
+          <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+
+            <div class="container">
+              <div class="carousel-caption">
+                <div class="row">
+                  <div class="col-md-12">
+                    <div class="text-bg">
+                      <span>Écouter, ça change tout</span>
+                      <h1>Des millions de titres et de podcasts.</h1>
+                      <a href="php/connexion.php">Connexion</a><a href="/php/inscription.php">Inscription </a>
+                      
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+
+            <div class="container ">
+              <div class="carousel-caption">
+
+                <div class="row">
+                  <div class="col-md-12">
+                    <div class="text-bg">
+                        <span>Le meilleur </span>
+                      <h1>Site de music</h1>
+                      <a href="#">Connexion</a><a href="#">Inscription </a>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+
+          
+     
+      <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+    <i class="fa fa-long-arrow-left" aria-hidden="true"></i>
+     
+    </a>
+    <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+        <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+      
+    </a>
+   </div>
+    
+  
+
+</section>
+</div>
+</header>
+
+
+
+<!-- about  -->
+<div id="about" class="about">
+  <div class="container">
+    <div class="row display_boxflex">
+      <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+        <div class="about-box">
+          <h2>About Us</h2>
+          <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look</p>
+          <a href="Javascript:void(0)">Read More</a>
+        </div>
+      </div>
+      <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+        <div class="about-box">
+          <figure><img src="images/about.png" alt="#" /></figure>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
+<!-- end abouts -->
+
+
+
+<?php 
+include('footer.php');
+?>
+
+
+</body>
+
+</html>
